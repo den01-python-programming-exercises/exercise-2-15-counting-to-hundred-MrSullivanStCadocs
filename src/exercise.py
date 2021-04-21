af7ff0d910ceceb595a8d9c172e7f8aa271c1d7a
MrSullivan <>
2021-04-21 20:31:38 +0000

    user input variable as int, i variable equal to user number, while  statement, comparable statement, print command, sum statement.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    userNumber = int(input(""))
+
+    i = userNumber
+    while (i<=100):
+      print(i)
+      i += 1
 
 if __name__ == '__main__':
     main()
